09a2591d6e25afef12d5718e3a753226465f965a
Gibson-Arbey <gibsonarbey@gmail.com>
2023-12-06 12:46:24 -0500

    Se realiza la configuracion de cors

--- src/main/java/co/edu/ufps/ayd/convocatoria/ConvocatoriaApplication.java
+++ src/main/java/co/edu/ufps/ayd/convocatoria/ConvocatoriaApplication.java
@@ -2,12 +2,29 @@ package co.edu.ufps.ayd.convocatoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:4200")
 public class ConvocatoriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConvocatoriaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
